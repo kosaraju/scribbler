5f19569c9633dc3536afa85df1643d4616c1b80b
kosaraju <gopala.kosaraju@ing.nl>
2019-12-21 11:45:16 +0100

    PartB postslist header added

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,17 +1,14 @@
 // Get the modal
 var signupmodel = document.getElementById("signupModal");
 var signinmodal = document.getElementById("signinModal");
-var newpostmodal = document.getElementById("inputNewPostModal");
 
 // Get the button that opens the modal
 var signupbtn = document.getElementById("signup");
 var signinbtn = document.getElementById("signin");
-var postbtn = document.getElementById("createposts");
 
 // Get the <i> element that closes the modal
 var signuptimes = document.getElementsByClassName("fas fa-times")[0];
 var signintimes = document.getElementsByClassName("fas fa-times")[1];
-var newposttimes = document.getElementsByClassName("fas fa-times")[2];
 
 // When the user clicks on the button, open the modal
 signupbtn.onclick = function() {
@@ -19,9 +16,6 @@ signupbtn.onclick = function() {
  }
 signinbtn.onclick = function() {
   signinmodal.style.display = "block";
-}
-postbtn.onclick = function() {
-  newpostmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -31,9 +25,6 @@ signuptimes.onclick = function() {
 signintimes.onclick = function() {
   signinmodal.style.display = "none";
 }
-newposttimes.onclick = function() {
-  newpostmodal.style.display = "none";
-}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == signupmodel) {
@@ -41,9 +32,6 @@ window.onclick = function(event) {
   }
   if (event.target == signinmodal) {
     signinmodal.style.display = "none";
-  }
-  if (event.target == newpostmodal) {
-    newpostmodal.style.display = "none";
   }
 }
 
@@ -60,10 +48,3 @@ function callSignup(){
   return;
 }
 
-function createPost(){
-  return;
-}
-
-function viewAllPosts() {
-  document.location.href="html/postslist.html";
-}
